93f7afe82d98f7983c8f595d8eb6428be28ba24a
xiaoran-MENG <meng.xiaoran@outlook.com>
2025-07-10 15:14:37 -0500

    Add jobs application

---
+++ apps/job-engine-jobs/src/main.ts
@@ -0,0 +1,25 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api';
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.setGlobalPrefix(globalPrefix);
+  app.use(cookieParser()); // Adds a middleware to parse incoming cookies and set them on the requet object
+  const port = app.get(ConfigService).getOrThrow('PORT');
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+}
+
+bootstrap();
