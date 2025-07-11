12fd4cdf4151a7602cef4da1b575c22fdfa2063d
xiaoran-MENG <meng.xiaoran@outlook.com>
2025-07-10 23:13:19 -0500

    Configure GRPC

--- apps/job-engine-auth/src/main.ts
+++ apps/job-engine-auth/src/main.ts
@@ -8,6 +8,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
+import { GrpcOptions, Transport } from '@nestjs/microservices';
+import { AUTH_PACKAGE_NAME } from 'types/proto/auth';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +19,15 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   app.use(cookieParser()); // Adds a middleware to parse incoming cookies and set them on the requet object
   const port = app.get(ConfigService).getOrThrow('PORT');
+  // GRPC
+  app.connectMicroservice<GrpcOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: AUTH_PACKAGE_NAME,
+      protoPath: join(__dirname, 'proto/auth.proto')
+    }
+  });
+  await app.startAllMicroservices();
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
